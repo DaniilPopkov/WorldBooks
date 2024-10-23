ed08e9f04d233d9b35e71b231b45e4c813ea2147
DaniilPopkov <popkovdaniil66@gmail.com>
2024-10-23 11:21:35 +0300

    23.10.2024 закончил на деталях  книги стр17 Попков

--- WebBooks/WebBooks/urls.py
+++ WebBooks/WebBooks/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from catalog import views
+from django.urls import re_path
 
 urlpatterns = [
     path("",views.index, name='home'),
     path('admin/', admin.site.urls),
+    path('',views.index,name='index'),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
 ]
