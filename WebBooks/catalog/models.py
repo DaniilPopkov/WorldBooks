b6bed0e307e75a186ab463f973fb1439e8140c7a
Your Name <you@example.com>
2024-12-27 14:51:35 +0300

    28.11.2024 Попков Якушева Лз19 научился выполнять администрирование сайта.

--- WebBooks/catalog/models.py
+++ WebBooks/catalog/models.py
@@ -17,6 +17,12 @@ class Language(models.Model):
     
     def __str__(self):
         return self.name
+class Publisher(models.Model):
+ name = models.CharField(max_length=20,
+ help_text=" Введите наименование издательства",
+ verbose_name="Издательство")
+ def __str__(self):
+  return self.name
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100, help_text="Введите имя автора",
@@ -42,6 +48,13 @@ class Book(models.Model):
         on_delete=models.CASCADE,
         help_text="Выберите язык книги",
         verbose_name="Язык книги", null=True)
+    publisher = models.ForeignKey('Publisher',
+        on_delete=models.CASCADE,
+        help_text="Выберите издательство",
+        verbose_name="Издательство", null=True)
+    year = models.CharField(max_length=4,
+        help_text="Введите год издания",
+        verbose_name="Год издания")
     author = models.ManyToManyField('Author',
         help_text="Выберите автора книги",
         verbose_name="Автор книги")
@@ -51,6 +64,12 @@ class Book(models.Model):
     isbn = models.CharField(max_length=13,
         help_text="Должно содержать 13 символов",
         verbose_name="ISBN книги")
+    price = models.DecimalField(decimal_places=2, max_digits=7,
+        help_text="Введите цену книги",
+        verbose_name="Цена (руб.)")
+    photo = models.ImageField(upload_to='images',
+        help_text="Введите изображение обложки",
+        verbose_name="Изображение обложки")
     
     def __str__(self):
         return self.title
@@ -72,25 +91,27 @@ class Status(models.Model):
         return self.name
 
 class BookInstance(models.Model):
-    book = models.ForeignKey('Book', on_delete=models.CASCADE,
-        null=True)
-    inv_nom = models.CharField(max_length=20, null=True,
+    book = models.ForeignKey('Book',
+        on_delete=models.CASCADE, null=True)
+    inv_nom = models.CharField(
+        max_length=20,
+        null=True,
         help_text="Введите инвентарный номер экземпляра",
         verbose_name="Инвентарный номер")
-    imprint = models.CharField(max_length=200,
-        help_text="Введите издательство и год выпуска",
-        verbose_name="Издательство")
-    status = models.ForeignKey('Status', on_delete=models.CASCADE,
-        null=True,
-        help_text='Изменить состояние экземпляра',
-        verbose_name="Статус экземпляра книги")
-    due_back = models.DateField(null=True, blank=True,
-        help_text="Введите конец срока статуса",
-        verbose_name="Дата окончания статуса")
-    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, 
-        null=True, blank=True, 
-        verbose_name="Заказчик", 
-        help_text='Выберите заказчика книги')
+    status = models.ForeignKey('Status',
+                               on_delete=models.CASCADE,
+                                null=True,
+                                help_text='Изменить состояние экземпляра',
+                                verbose_name="Статус экземпляра книги")
+    due_back = models.DateField(null=True,
+                                blank=True,
+                                help_text="Введите конец срока статуса",
+                                verbose_name="Дата окончания статуса")
+    class Meta:
+       ordering = ["due_back"]
+       def __str__(self):
+         return '%s %s %s' % (self.inv_nom, self.book, self.status)
+       
     @property 
     def is_overdue(self): 
         if self.due_back and date.today() > self.due_back: 
